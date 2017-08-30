c83d712da7c83ba3f1e66e41d2501b21665383ef
MRLmic <missi.luna@gmail.com>
2017-08-30 18:53:05 -0400

    Added Turn Change Function
    Function enters O or X in array d/t modulo.

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,11 +1,17 @@
 let currentGameArray = ['', '', '', '', '', '', '', '', '']
 let accumulator = 0
-const character = 'X'
+let character
 const fillArray = function (i) {
-  if (currentGameArray[i] !== 'X' && 'O') {
+  if (currentGameArray[i] === '') {
     currentGameArray[i] = character
     accumulator += 1
   } else {
     console.log('Please choose another square.')
   }
+  turnChange()
+}
+const turnChange = function () {
+  if (accumulator % 2 === 0) {
+    character = 'X'
+  } else { character = 'O' }
 }
