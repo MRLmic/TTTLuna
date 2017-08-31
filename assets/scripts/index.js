097bfde5c47a5fd38cbc6bab76ed2026fa5af535
MRLmic <missi.luna@gmail.com>
2017-08-31 15:58:59 -0400

    Code moved to index.js to resolve async probs.
    Squares filling in, turns switching. Everything is fine.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,13 +2,66 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+// const needFunctions = require('./events')
 
+// $(() => {
+//   setAPIOrigin(location, config)
+// })
+//
+// // use require with a reference to bundle the file and use it in this file
+// // const example = require('./example')
+//
+// // use require without a reference to ensure a file is bundled
+// // require('./example')
+// let currentID
+// $("#wrapper").children().on("click", function (event) {
+//   console.log($(event.target).attr('id'))
+//   currentID = $(event.target).attr('id')
+//   needFunctions.fillArray(currentID)
+//   console.log(needFunctions.character)
+//   $(event.target).text(needFunctions.character)
+// })
+//
+// module.exports = {
+//   currentID
+// }
+// $(() => {
+//   setAPIOrigin(location, config)
+// })
+
+// wrap it all in an onPageLoad
 $(() => {
-  setAPIOrigin(location, config)
+  let currentGameArray = ['', '', '', '', '', '', '', '', '']
+  let accumulator = 2
+  let character = 'X'
+
+  const turnChange = function () {
+    if (accumulator % 2 === 0) {
+      character = 'X'
+      console.log('X is up!')
+    } else {
+      character = 'O'
+      console.log('O is up!')
+    }
+  }
+
+  const fillArray = function (currentID) {
+    if (currentGameArray[currentID] === '') {
+      accumulator += 1
+      turnChange()
+      console.log(currentID)
+      currentGameArray[currentID] = character
+    } else {
+      console.log('Please choose another square.')
+    }
+  }
+
+  let currentID
+  $("#wrapper").children().on("click", function (event) {
+    console.log($(event.target).attr('id'))
+    currentID = $(event.target).attr('id')
+    $(event.target).text(character)
+    fillArray(currentID)
+    console.log(character)
+  })
 })
-
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-
-// use require without a reference to ensure a file is bundled
-// require('./example')
