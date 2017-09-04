67e8f0ff282f364552df7b04e7ddbaaa9d5288e8
MRLmic <missi.luna@gmail.com>
2017-09-04 13:11:44 -0400

    Changes up to changePassword success

---
+++ assets/scripts/api.js
@@ -0,0 +1,33 @@
+const config = require('../../config')
+const store = require('./store')
+
+const create = function (data) {
+  console.log('reached create function')
+  return $.ajax({
+    url: config.apiOrigin + '/sign-up/',
+    method: 'POST',
+    data
+  })
+}
+const logIn = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/sign-in/',
+    method: 'POST',
+    data
+  })
+}
+const changePassword = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/change-password/' + store.id,
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.token
+    },
+    data
+  })
+}
+module.exports = {
+  create,
+  logIn,
+  changePassword
+}
