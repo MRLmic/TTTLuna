26eb6368a7e577843255b7607a27cf28f9198517
MRLmic <missi.luna@gmail.com>
2017-09-04 13:13:45 -0400

    Sign-in sh script added

---
+++ assets/scripts/sign-inJSON.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# sh scripts/sign-in-json.sh
+
+API="${API_ORIGIN:-http://tic-tac-toe.wdibos.com}"
+URL_PATH="/sign-in"
+# API="${API_ORIGIN:-https://aqueous-atoll-85096.herokuapp.com/"
+# URL_PATH="/sign-in"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
